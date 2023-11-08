e937d9e71dcd897ecae0950e7911f63fccda6099
Siuu <emilbur@chalmers.se>
2023-11-08 16:49:07 +0100

    Changes by Anton

--- Cars.java
+++ Cars.java
@@ -32,9 +32,53 @@ public class Cars implements Movable {
         currentSpeed = Math.min(getCurrentSpeed() + speedFactor() * amount,enginePower);}
 
     private double speedFactor() { return 0; }
+    public void gas(double amount){
+        incrementSpeed(amount);
+    }
+
+    // TODO fix this method according to lab pm
+    public void brake(double amount){
+        decrementSpeed(amount);
+    }
 
     public void decrementSpeed(double amount){
         currentSpeed = Math.max(getCurrentSpeed() - speedFactor() * amount,0);
     }
+    private double x_coordinate = 0;
+    private double y_coordinate = 0;
 
+    public void move() {
+        while (getCurrentSpeed()!=0 && direction == 1 || direction == 3){
+            if (direction == 1){
+                x_coordinate -= getCurrentSpeed();
+            }
+            if (direction == 3){
+                x_coordinate += getCurrentSpeed();
+            }
+        }
+        while (getCurrentSpeed()!=0 && direction == 4||direction== 2){
+            if (direction == 4){
+                y_coordinate -= getCurrentSpeed();
+            }
+            if (direction == 2){
+                y_coordinate += getCurrentSpeed();
+            }
+        }
+    }
+    public void turnLeft() {
+
+        direction = direction-1;
+        if (direction == 0) {
+            direction = 4;
+        }
+    }
+
+
+    public void turnRight() {
+
+        direction += 1;
+        if (direction==4){
+            direction = 1;
+        }
+    }
 }
