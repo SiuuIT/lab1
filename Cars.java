02f5e97ecb21ebb1624422abd196fc23df58ebe4
wenji <leeniley@hotmail.com>
2023-11-10 11:34:17 +0100

    new

--- Cars.java
+++ Cars.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+
 public class Cars implements Movable {
     public int direction = 2;
     public int nrDoors; // Number of doors on the car
@@ -6,89 +7,80 @@ public class Cars implements Movable {
     public double currentSpeed; // The current speed of the car
     public Color color; // Color of the car
     public String modelName; // The car model name
-
-
-    public int getNrDoors() {
+    public int getNrDoors(){
         return nrDoors;
     }
-
-    public double getEnginePower() {
+    public double getEnginePower(){
         return enginePower;
     }
-
-    public double getCurrentSpeed() {
+    public double getCurrentSpeed(){
         return currentSpeed;
     }
-
-    public Color getColor() {
+    public Color getColor(){
         return color;
     }
-
-    public void setColor(Color clr) {
+    public void setColor(Color clr){
         color = clr;
     }
-
-    public void startEngine() {
+    public void startEngine(){
         currentSpeed = 0.1;
     }
-
-    public void stopEngine() {
+    public void stopEngine(){
         currentSpeed = 0;
     }
+    public void incrementSpeed(double amount){
+        currentSpeed = Math.min(getCurrentSpeed() + speedFactor() * amount,enginePower);}
 
-    public void incrementSpeed(double amount) {
-        currentSpeed = Math.min((getCurrentSpeed() + (speedFactor() * amount)), enginePower);
+    private double speedFactor() { return 0; }
+    public void gas(double amount){
+        amount = Math.max(0, Math.min(1,amount));
+        incrementSpeed(amount);
     }
 
-    private double speedFactor() {
-        return 0;
-
-    }
-    public void gas(double amount) {
-        if(0.0D <= amount && amount <= 1.0D) {
-            incrementSpeed(amount);
-        }
+    // TODO fix this method according to lab pm
+    public void brake(double amount){
+        amount = Math.max(0, Math.min(1,amount));
+        decrementSpeed(amount);
     }
 
-    public void brake(double amount) {
-        if(0.0D <= amount && amount <= 1.0D) {
-            decrementSpeed(amount);}
+    public void decrementSpeed(double amount){
+        currentSpeed = Math.max(getCurrentSpeed() - speedFactor() * amount,0);
     }
-
-    public void decrementSpeed(double amount) {
-        currentSpeed = Math.max((getCurrentSpeed() - (speedFactor() * amount)), 0);
-    }
-
-    public double x_coordinate = 0;
-    public double y_coordinate = 0;
+    private double x_coordinate = 0;
+    private double y_coordinate = 0;
 
     public void move() {
-        if (getCurrentSpeed() != 0 && direction == 1 || direction == 3) {
-            if (direction == 1) {
+        while (getCurrentSpeed()!=0 && (direction == 1 || direction == 3)){
+            if (direction == 1){
                 x_coordinate -= getCurrentSpeed();
-            if (direction == 3) {
-                x_coordinate += getCurrentSpeed();
-                }
             }
-        if (getCurrentSpeed() != 0 && direction == 4 || direction == 2) {
-            if (direction == 4) {
+            if (direction == 3){
+                x_coordinate += getCurrentSpeed();
+            }
+        }
+        while (getCurrentSpeed()!=0 && (direction == 4||direction== 2)){
+            if (direction == 4){
                 y_coordinate -= getCurrentSpeed();
-                }
-            if (direction == 2) {
-                y_coordinate = y_coordinate+ getCurrentSpeed();
-                }
+            }
+            if (direction == 2){
+                y_coordinate += getCurrentSpeed();
             }
         }
     }
     public void turnLeft() {
-        direction = direction - 1;
+
+        direction = direction-1;
         if (direction == 0) {
-            direction = 4; }
+            direction = 4;
+        }
     }
+
+
     public void turnRight() {
+
         direction += 1;
-        if (direction == 5) {
-            direction = 1;}
+        if (direction==4){
+            direction = 1;
+        }
     }
 }
-
